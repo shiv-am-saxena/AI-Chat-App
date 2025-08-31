c89c4f6aaff5af67c22e61ce2f816b2c769e292b
Shivam Saxena <shivam2003official@gmail.com>
2025-08-31 17:05:54 +0530

    fix: Remove commented-out dashboard route for cleaner code

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -64,7 +64,6 @@ export default function AppRouter() {
                 <Route path={'sign-up'} element={<Signup />} />
             </Route>
             <Route path={'/dashboard'} element={isAuthenticated ? <Dashboard /> : <Navigate to={'/auth/sign-in'} replace/>} /> Protected Route 
-            {/* <Route path={'/dashboard'} element={<Dashboard />}/>  */}
             <Route path={'/error'} element={<Error404 />} />
             <Route path={'*'} element={<Navigate to={'/error'} replace />} />
         </Routes>
